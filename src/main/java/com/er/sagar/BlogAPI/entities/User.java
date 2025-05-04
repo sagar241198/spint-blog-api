d28872a167677046f0d2f140c253ec4c1225e06a
sagar241198 <78961827+sagar241198@users.noreply.github.com>
2025-05-04 21:26:19 +0530

    cahnge db and schecma

--- src/main/java/com/er/sagar/BlogAPI/entities/User.java
+++ src/main/java/com/er/sagar/BlogAPI/entities/User.java
@@ -5,14 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Long id;  // Changed from int to Long
 
     @Column(unique = true)
     private String email;
@@ -37,11 +39,11 @@ public class User {
     }
 
     // Getters and Setters
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
